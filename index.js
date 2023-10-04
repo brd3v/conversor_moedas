3b57ea1a4d2f56f523e46be3d66aeba19fe58858
brd3v <bnsiq2015@gmail.com>
2023-10-04 00:19:47 -0300

    conversor de real para dolar

---
+++ index.js
@@ -0,0 +1,20 @@
+const moeda = document.getElementById('reais')
+const action = document.getElementById('btn')
+const result = document.getElementById('result')
+const cotacao = document.getElementById('cotacao')
+
+fetch('https://economia.awesomeapi.com.br/last/USD-BRL')
+.then((r) => r.json())
+.then( (data) => {
+let retorno = data.USDBRL
+action.addEventListener('click',() => {
+    let count = moeda.value / retorno.high
+        result.innerHTML =`resultado da conversão é: $${count.toFixed(2)}`
+    
+})
+
+
+cotacao.innerHTML = ` a cotação atual do Dolar é: $${retorno.high}`
+
+    console.log(retorno.high)})
+.catch((error) => console.log(error))
